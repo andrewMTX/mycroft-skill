aa6c1ead37fe2b8512100eef13c45cdd6c81af16
Parallels <andrew.smith@mtxb2b.com>
2018-11-01 22:12:29 -0400

    ifix date

--- __init__.py
+++ __init__.py
@@ -100,8 +100,7 @@ class TemplateSkill(MycroftSkill):
         json_data = json.loads(response.text)
         latest_appointment = len(json_data['entry'])-1
         start_date = json_data['entry'][latest_appointment]['resource']['start']
-        sd = dateutil.parser.parse(start_date)
-        sdo = time.strftime("%b %d %Y %H %M", time.gmtime(sd))
+        sdo = datetime.datetime.strptime(start_date,'%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d at %H %M am')
 
         #print(json_data['entry'][latest_appointment]['resource']['end'])
         place = json_data['entry'][latest_appointment]['resource']['participant'][0]['actor']['display']
